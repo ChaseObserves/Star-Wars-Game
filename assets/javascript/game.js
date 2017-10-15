059d4d5a65b1a2293284303326aa4d18d7fc7286
Chase Adams <chaseme37@MacBook-Pro.local>
2017-10-14 23:53:56 -0600

    started over and barely got it in before midnight

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,97 +1,260 @@
 // Prepare document for jQuery and run after HTML loads
-$( document ).ready(function() {
-    console.log( "ready!" );
-
+$(document).ready(function() {
 // GLOBAL VARIABLES
 //---------------------------------------------------
-var characters = {
-	"bobafett": {
-		name: "Boba Fett",
-		healthpoints: 120,
-		attackpower: 8,
-		counterattack: 15,
-	},
-	"chewy": {
-		name: "Chewy",
-		healthpoints: 180,
-		attackpower: 17,
-		counterattack: 4,
-	},
-	"darthvader": {
-		name: "Darth Vader",
-		healthpoints: 140,
-		attackpower: 13,
-		counterattack: 20,
-	},
-	"luke": {
-		name: "Luke",
-		healthpoints: 140,
-		attackpower: 15,
-		counterattack: 17,
-	},
-	"obiwan": {
-		name: "Obi Wan",
-		healthpoints: 110,
-		attackpower: 13,
-		counterattack: 17,
-	},
-	"yoda": {
-		name: "Yoda",
-		healthpoints: 90,
-		attackpower: 12,
-		counterattack: 21,
-	},
-}
+	var characters = {
+		"Boba Fett": {
+			name: "Boba Fett",
+			healthpoints: 120,
+			attackpower: 8,
+			counterattack: 15,
+			imageUrl: "assets/images/bobafett.png"
+		},
+		"Chewy": {
+			name: "Chewy",
+			healthpoints: 180,
+			attackpower: 17,
+			counterattack: 4,
+			imageUrl: "assets/images/chewy.png"
+		},
+		"Darth Vader": {
+			name: "Darth Vader",
+			healthpoints: 140,
+			attackpower: 13,
+			counterattack: 20,
+			imageUrl: "assets/images/darthvader.png"
+		},
+		"Luke": {
+			name: "Luke",
+			healthpoints: 140,
+			attackpower: 15,
+			counterattack: 17,
+			imageUrl: "assets/images/luke.png"
+		},
+		"Obi Wan": {
+			name: "Obi Wan",
+			healthpoints: 110,
+			attackpower: 13,
+			counterattack: 17,
+			imageUrl: "assets/images/obiwan.png"
+		},
+		"Yoda": {
+			name: "Yoda",
+			healthpoints: 90,
+			attackpower: 12,
+			counterattack: 21,
+			imageUrl: "assets/images/yoda.png"
+		}
+	};
 
-var chosenCharacter
-var chosenOpponent
-var allOpponents
-
-
-
-
-
-
-
-
-
-
-
-
-
+	//Will be populated when the player selects a character.
+	var currSelectedCharacter;
+	// Populated with all characters player did not pick.
+	var combatants = [];
+	// Populated when player picks an opponent.
+	var currDefender;
+	// Will keep track of damage and turns.
+	var turnCounter = 1;
+	// Tracks number of defeated opponents.
+	var killCount = 0;
 
 
 
 // FUNCTIONS
 //---------------------------------------------------
 
+	// This function will render the characters to the page.
+	// The character rendered and the area they are rendered to.
+	var renderOne = function(character, renderArea, charStatus) {
+		var charDiv = $("<div class='character' data-name='" + character.name + "'>");
+		var charName = $("<div class='character-name'>").text(character.name);
+		var charImage = $("<img class='character-image'>").attr("src", character.imageUrl);
+		var charHealth = $("<div class='character-health'>").text(character.healthpoints);
+		charDiv.append(charName).append(charImage).append(charHealth);
+		$(renderArea).append(charDiv);
 
+		// If the character is an enemy or defender (the active opponent), add the appropriate class.
+		if (charStatus === "enemy") {
+			$(charDiv).addClass("enemy");
+		}
+		else if (charStatus === "defender") {
+			// Populate currDefender with the selected opponents information.
+			currDefender = character;
+			$(charDiv).addClass("target-enemy");
+		}
+	};
 
-var applyClasses = function() {
-	// Assign whichever fighter is clicked first to the chosenCharacter variable
-	$(".fighter").off().on("click", function() {
-		chosenCharacter = this;
-		$(chosenCharacter).addClass('player1').removeClass('fighter');
-		$('.fighter').addClass('opponent').removeClass('fighter');
-		$("#choose").html("<h3>CHOOSE YOUR OPPONENT</h3>");
-		$("#attacker").append($(this));
-		applyClasses();
-		console.log(chosenCharacter);
+	var renderMessage = function (message) {
+		// Builds the message and appends it to the page.
+		var gameMessageSet = $("#game-message");
+		var newMessage = $("<div>").text(message);
+		gameMessageSet.append(newMessage);
+		// If we get this specific message passed in, clear the message
+		if (message === "clearMessage") {
+			gameMessageSet.text("");
+		}
+	}
+
+	var renderCharacters = function(charObj, areaRender) {
+		// "characters-section" is the div where all our characters begin
+		// If true, render all characters to the starting area.
+		if (areaRender === "#characters-section") {
+			$(areaRender).empty();
+			// Loop through the characters object and call the renderOne function.
+			for (var key in charObj) {
+				if (charObj.hasOwnProperty(key)) {
+					renderOne(charObj[key], areaRender, "");
+				}
+			}
+		}
+
+		// "selected-character" is the div where our selected character a
+		// If true, render the selected player character to this area.
+		if (areaRender === "#selected-character") {
+			renderOne(charObj, areaRender, "");
+		}
+
+		// "available-to-attack" is the div where our "inactive" opponent
+		// If true, render the selected character to this area
+		if (areaRender === "#available-to-attack-section") {
+			// Loop through the combatants array and call the renderOne function.
+			for (var i = 0; i < charObj.length; i++) {
+				renderOne(charObj[i], areaRender, "enemy");
+			}
+			// Creates an on click event for each enemy.
+			$(document).on("click", ".enemy", function() {
+				var name = ($(this).attr("data-name"));
+
+				// If there is no defender, the clicked enemy will become the defender.
+				if ($("#defender").children().length === 0) {
+					renderCharacters(name, "#defender");
+					$(this).hide();
+					renderMessage("clearMessage");
+				}
+			});
+		}
+
+		// "defender" is the div where the active opponent appears.
+		// If true, render the selected enemy in this location.
+		if (areaRender === "#defender") {
+			$(areaRender).empty();
+			for (var i = 0; i < combatants.length; i++) {
+				if (combatants[i].name === charObj) {
+					renderOne(combatants[i], areaRender, "defender");
+				}
+			}
+		}
+
+		// Re-render defender when attacked.
+		if (areaRender === "playerDamage") {
+			$("#defender").empty();
+			renderOne(charObj, "#defender", "defender");
+		}
+
+		// Re-render player when counterattacked.
+		if (areaRender === "enemyDamage") {
+			$("#selected-character").empty();
+			renderOne(charObj, "#selected-character", "");
+		}
+
+		// Remove defeated enemy.
+		if (areaRender === "enemyDefeated") {
+			$("#defender").empty();
+			var gameStateMessage = "You have defeated " + charObj.name + ", choose your next opponent.";
+			renderMessage(gameStateMessage);
+		}
+	};
+
+	// Function which handles restarting the game after defeat or victory.
+	var restartGame = function(inputEndGame) {
+		
+		// When the restart button is clicked, reload the page.
+		var restart = $("<button>Restart</button>").click(function() {
+			location.reload();
+		});
+
+		// Build div that will display the victory/defeat message
+		var gameState = $("<div>").text(inputEndGame);
+
+		// Render the restart button and victory/defeat message to page.
+		$("body").append(gameState);
+		$("body").append(restart);
+	};
+
+	$(document).on("click", ".character", function() {
+		var name = $(this).attr("data-name");
+		// If a player has not yet been chosen...
+		if (!currSelectedCharacter) {
+			// We populate currSelectedCharacter with the selected character's information
+			currSelectedCharacter = characters[name];
+			// We then loop through the remaining characters and push them to the combatants array.
+			for (var key in characters) {
+				if (key !== name) {
+					combatants.push(characters[key]);
+				}
+			}
+
+			console.log(combatants);
+			// Hide the character select div.
+			$("#characters-section").hide();
+
+			// Then render our selected character and our combatants.
+			renderCharacters(currSelectedCharacter, "#selected-character");
+			renderCharacters(combatants, "#available-to-attack-section");
+		}
 	});
 
-// Change "Choose Your Fighter" to "Choose Your Opponent" and assign choice to chosenOpponent (prevent same choice twice)
-	$(".opponent").off().on("click", function() {
-		chosenOpponent = this;
-		$(chosenOpponent).addClass('player2').removeClass('opponent');
-		$('.opponent').addClass('others animated fadeOutDown').removeClass('opponent');
-		$("#choose").html("<h3>FIGHT!</h3>");
-		$("#defender").append($(this));
-		$('#character-select').fadeOut(1000);
-		$('#attack-button').removeClass('hide');
-		applyClasses();
-		console.log(chosenOpponent);
+	// When you click the attack button, run the following game logic...
+	$("#attack-button").on("click", function() {
+		if ($("#defender").children().length !== 0) {
+
+			// Creates messages for player's attack and opponent counterattack
+			var attackMessage = "You attacked " + charObj.name + " for " + (currSelectedCharacter.attack * turnCounter) + " damage.";
+			var counterAttackMessage = currDefender.name + " attacked you back for " + currDefender.enemyCounterAttack + " damage.";
+			renderMessage("clearMessage");
+
+
+			// Reduce enemy health by player attack value
+			currDefender.healthpoints -= (currSelectedCharacter.attack * turnCounter)
+
+			// If the enemy still has health...
+			if (currDefender.healthpoints > 0) {
+
+				// Render the enemy's updated character icon.
+				renderCharacters(currDefender, "playerDamage");
+
+				// Render the combat messages.
+				renderMessage(attackMessage);
+				renderMessage(counterAttackMessage);
+
+				// Reduce your health by the opponent's counter attack value.
+				currSelectedCharacter.healthpoints -= currDefender.enemyCounterAttack;
+
+				// Render the player's updated character card
+				renderCharacters(currSelectedCharacter, "enemyDamage");
+
+				// If player has less than zero health the game ends
+				if (currSelectedCharacter.health <= 0) {
+					renderMessage("clearMessage");
+					restartGame("You have been killed");
+					$("attack-button").unbind("click");
+				} 
+			}
+			else {
+				// Remove character icon
+				renderCharacters(currDefender, "enemyDefeated");
+				// Increment the kill count
+				killCount++;
+				// If all opponents have been killed, player wins
+				if (killCount >= 5) {
+					renderMessage("clearMessage");
+					renderMessage("You Won!");
+				}
+			}
+		}
+		turnCounter++;
 	});
-}
+		
 
 
 
@@ -108,62 +271,6 @@ var applyClasses = function() {
 // MAIN PROCESSES
 //---------------------------------------------------
 
-
-applyClasses();
-
-})
-
-
-
-
-
-
-
-
-
-
-// No matter what I do, I can't get the first on click event to stop running/assigning new characters
-// into the chosenCharacter variable with each subsequent click.
-
-// Also, I'm not 100% sure how I'm going to connect the divs I've created for each character in the HTML
-// document with the characters array I've created in here with objects and properties that I'll need for
-// the fighting to work
-
-
-
-
-// * When the game starts, the player will choose a character by clicking on the fighter's picture. The player will fight as that character for the rest of the game.
-
-//    * The player must then defeat all of the remaining fighters. Enemies should be moved to a different area of the screen.
-
-//    * The player chooses an opponent by clicking on an enemy's picture.
-
-//    * Once the player selects an opponent, that enemy is moved to a `defender area`.
-
-//    * The player will now be able to click the `attack` button.
-//      * Whenever the player clicks `attack`, their character damages the defender. The opponent will lose `HP` (health points). These points are displayed at the bottom of the defender's picture. 
-//      * The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture.
-
-// 3. The player will keep hitting the attack button in an effort to defeat their opponent.
-
-//    * When the defender's `HP` is reduced to zero or below, remove the enemy from the `defender area`. The player character can now choose a new opponent.
-
-// 4. The player wins the game by defeating all enemy characters. The player loses the game the game if their character's `HP` falls to zero or below.
-
-// ##### Option 2 Game design notes
-
-// * Each character in the game has 3 attributes: `Health Points`, `Attack Power` and `Counter Attack Power`.
-
-// * Each time the player attacks, their character's Attack Power increases by its base Attack Power. 
-//   * For example, if the base Attack Power is 6, each attack will increase the Attack Power by 6 (12, 18, 24, 30 and so on).
-// * The enemy character only has `Counter Attack Power`. 
-
-//   * Unlike the player's `Attack Points`, `Counter Attack Power` never changes.
-
-// * The `Health Points`, `Attack Power` and `Counter Attack Power` of each character must differ.
-
-// * No characters in the game can heal or recover Health Points. 
-
-//   * A winning player must pick their characters wisely by first fighting an enemy with low `Counter Attack Power`. This will allow them to grind `Attack Power` and to take on enemies before they lose all of their `Health Points`. Healing options would mess with this dynamic.
-
-// * Your players should be able to win and lose the game no matter what character they choose. The challenge should come from picking the right enemies, not choosing the strongest player.
+// Render all characters to the page when the game starts
+	renderCharacters(characters, "#characters-section");
+});
